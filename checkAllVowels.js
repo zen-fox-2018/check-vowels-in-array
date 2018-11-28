9f9f86c7b8bf559f64f08cdca1c1ad1c7409b7af
kangman53 <hermanboy53@gmail.com>
2018-11-28 16:43:32 +0700

    check vowels in array done

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -21,6 +21,28 @@ function generateBoard (jumlahRow, jumlahCol) {
 //   ['P', 'D', 'A', 'I']
 // ];
 
+// MENGGUNAKAN BUILT IN FUNCTION
+// function checkBlock(board) {
+//   let myResult = [];
+//   for (let i = 0; i < board.length; i++) {
+//     for (let j = 0; j < board[i].length; j++) {
+//       let checkSquare = [];
+//       if (i - 1 >= 0 && j - 1 >= 0) {
+//         debugger;
+//         checkSquare.push(board[i-1][j-1], board[i-1][j], board[i][j-1], board[i][j]);
+//         let isCheck = checkSquare.every(data => {
+//           let vowels = ['A', 'I', 'U', 'E', 'O']
+//           return vowels.includes(data);
+//         });
+//         if(isCheck){
+//           myResult.push(checkSquare);
+//         }
+//       }
+//     }
+//   }
+//   return myResult;
+// }
+
 function checkBlock(board) {
   let myResult = [];
   for (let i = 0; i < board.length; i++) {
@@ -29,10 +51,7 @@ function checkBlock(board) {
       if (i - 1 >= 0 && j - 1 >= 0) {
         debugger;
         checkSquare.push(board[i-1][j-1], board[i-1][j], board[i][j-1], board[i][j]);
-        let isCheck = checkSquare.every(data => {
-          let vowels = ['A', 'I', 'U', 'E', 'O']
-          return vowels.includes(data);
-        });
+        let isCheck = everyManual (checkSquare);
         if(isCheck){
           myResult.push(checkSquare);
         }
@@ -42,8 +61,28 @@ function checkBlock(board) {
   return myResult;
 }
 
+function everyManual (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (includesManual(arr[i]) === false) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function includesManual (data) {
+  let vowels = ['A', 'I', 'U', 'E', 'O']
+  let isCheck = false;
+  for (let i = 0; i < vowels.length; i++) {
+    if (data === vowels[i]) {
+      return true;
+    }
+  }
+  return false;
+}
+
 function checkAllVowels(jumlahRow, jumlahCol) {
   let myResult = checkBlock(generateBoard(jumlahRow, jumlahCol));
-  return `Jumlah block data adalah ${myResult.length}`;
+  return `Jumlah block data adalah ${myResult.length}` + myResult;
 }
-console.log(checkAllVowels(10,10));
+console.log(checkAllVowels(15,15));
