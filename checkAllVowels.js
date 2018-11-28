8951ef1113ca790f67ca309937eb4af152ca44fc
ChristianHS23 <christian.sihotang23@gmail.com>
2018-11-28 11:30:33 +0700

    rapiin

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -2,12 +2,12 @@
 
 function checkVowel(inputArray){
     var output = []
-    for(let i=0; i < inputArray.length; i++){
+    for(let i=0; i < inputArray.length; i++) {
        
-        for(let j=0; j < inputArray[i].length; j++){
+        for(let j=0; j < inputArray[i].length; j++) {
             let temp = []
             if(inputArray[i+1]){
-                if(checkIsVowel(inputArray[i][j]) && checkIsVowel(inputArray[i][j+1]) && checkIsVowel(inputArray[i+1][j]) &&  checkIsVowel(inputArray[i+1][j+1])){
+                if(checkIsVowel(inputArray[i][j]) && checkIsVowel(inputArray[i][j+1]) && checkIsVowel(inputArray[i+1][j]) &&  checkIsVowel(inputArray[i+1][j+1])) {
                     temp.push(inputArray[i][j], inputArray[i][j+1] , inputArray[i+1][j], inputArray[i+1][j+1])
                     output.push(temp)
                 }
@@ -18,19 +18,19 @@ function checkVowel(inputArray){
     return output
 }
 
-function checkIsVowel(string){
+function checkIsVowel(string) {
     var kamus = 'AIUEO'
-    for(let k=0; k < kamus.length; k++){
+    for(let k=0; k < kamus.length; k++) {
         if(string == kamus[k]) return true
     }
     return false
 }
 
-function generateSquare (row,col){
+function generateSquare (row,col) {
    let  output = []
-    for(let i=0; i < row; i++){
+    for(let i=0; i < row; i++) {
         let outputDalam = []
-        for(let j=0; j < col; j++){
+        for(let j=0; j < col; j++) {
             let randomString = String.fromCharCode(~~((Math.random() * 26 ) + 65))
             outputDalam.push(randomString)
         }
