94b4d68128e1ea39b27c5286049a03a325ec6913
Zhang <zhang@ZhangMBP15.localdomain>
2018-11-28 17:50:27 +0700

    ubah nama var

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -2,7 +2,6 @@ function random() {
   let abzad = 'abcdefghijklmnopqrstuvwxyz'
   return abzad[Math.floor(Math.random()*abzad.length)]
 }
-// console.log(random())
 
 function generateBoard(row, col) {
   let result = [];
@@ -16,24 +15,18 @@ function generateBoard(row, col) {
   }
   return result
 }
-// console.log(generateBoard(5,4))
 
 function checkVowels(data) {
   if(data == 'a' || data == 'i' || data == 'u' || data == 'e' || data == 'o') return true;
   return false
 }
-// console.log(checkVowels('e'))
 
 function checkKotak(data) {
   let counter = 0;
-  // console.log(data.length)
   
-   data.forEach((i , indexi) => {
-    i.forEach((j , indexj) => {
-      // console.log(data[indexi].length)
+   data.forEach((arr , indexi) => {
+    arr.forEach((char , indexj) => {
       if(indexi < data.length-1 && indexj < data[indexi].length-1 ) {
-        // console.log('====== ini i' , indexi , '\n')
-        // console.log('====== ini j' , indexj)
         if(checkVowels(data[indexi][indexj]) && checkVowels(data[indexi][indexj+1]) && checkVowels(data[indexi + 1][indexj]) && checkVowels(data[indexi +1][indexj + 1])){
           counter++
         }
